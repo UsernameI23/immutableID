ac13523be355192e295918bdf6e5cf422e49fae9
Randy Miller <Randy Miller@DESKTOP-7SQA22L>
2023-03-06 23:33:00 -0500

    Add project files.

---
+++ immutableID/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+
+class Student
+{
+    // auto-implemented properties
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // default constructor
+    public Student()
+    {
+        Id = 0;
+        FirstName = "";
+        LastName = "";
+    }
+    // parameterized constructor
+
+    public Student(int i, string First, string Last)
+    {
+        Id = i;
+        FirstName = First;
+        LastName = Last;
+    }
+    public static void Main()
+    {
+        Student student = new(32,"Emma", "Madison");
+        Console.WriteLine(student);
+        Student student2 = new();
+        student2.Id = 45;
+        student2.FirstName = "James";
+        student2.LastName = "Williams";
+        Console.WriteLine(student2);
+
+        
+
+    }
+}
